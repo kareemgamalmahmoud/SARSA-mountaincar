a3a746a19bd34d66f56f51ff66d1823d291f9be6
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2021-04-19 18:46:00 +0200

    Add gradient update function

--- src/SARSA.py
+++ src/SARSA.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import random
+from src.mountaincar import Actions
 from typing import Optional, Tuple
 
 import numpy as np
@@ -46,7 +47,7 @@ class SARSA:
         self.discount_factor = parameters.SARSA_DISCOUNT_FACTOR
         self.trace_decay = parameters.SARSA_TRACE_DECAY
 
-        if model_name is None:
+        if model_path is None:
             self.learning_rate = parameters.SARSA_LEARNING_RATE
             self.dimensions = parameters.SARSA_DIMENSIONS
             self.activation_function = parameters.SARSA_ACTIVATION_FUNCTION
@@ -57,6 +58,7 @@ class SARSA:
         else:
             self.load(model_path)
 
+        self.reset_eligibilities()
         self.epsilon_history = []
 
     def build_model(self) -> Sequential:
@@ -66,13 +68,15 @@ class SARSA:
         self.name = 'Training model'
         input_dim, *hidden_dims, output_dim = self.dimensions
 
+        assert output_dim == 1, 'Output dimension must be 1'
+
         model = Sequential()
         model.add(Input(shape=(input_dim,)))
 
         for dimension in hidden_dims:
             model.add(Dense(dimension, activation=self.activation_function))
 
-        model.add(Dense(output_dim, activation=softmax))
+        model.add(Dense(units=1, activation='linear'))
 
         model.compile(
             optimizer=(self.optimizer(learning_rate=self.learning_rate) if self.learning_rate is not None else self.optimizer()),
@@ -97,24 +101,43 @@ class SARSA:
     def choose_uniform(self) -> int:
         raise NotImplementedError
 
-    def choose_greedy(self, state: Tuple[int, ...]) -> int:
-        action_probabilities = np.array(self.Q(tf.convert_to_tensor([state])))
-        return np.argmax(action_probabilities)
+    def choose_greedy(self, state: Tuple[int, ...]) -> Actions:
+        action_probabilities = []
+        for action in [-1, 0, 1]:
+            action_probabilities.append(float(self.Q(tf.convert_to_tensor([state, action]))))  # type: ignore
+        return -1
 
-    def choose_softmax(self, state: Tuple[int, ...], temperature: int) -> int:
-        action_probabilities = np.array(self.Q(tf.convert_to_tensor([state])))        action_probabilities = action_probabilities * np.array(valid_actions)
-        action_probabilities = softmax_v2(action_probabilities, temperature)
-        return np.random.choice(range(0, 10), 1, p=action_probabilities)[0]  # ??
+    def choose_softmax(self, state: Tuple[int, ...], temperature: int) -> Actions:
+        # action_probabilities = np.array(self.Q(tf.convert_to_tensor([state])))
+        # action_probabilities = softmax_v2(action_probabilities, temperature)
+        # return np.random.choice(range(0, 10), 1, p=action_probabilities)[0]  # ??
+        raise NotImplementedError
 
-    def fit(self) -> None:
-        # Same as in NN-criitc?
-        # history = self.Q.fit(X, Y)
+    def update(self, reward: float, successor_state: Tuple[int], current_state: Tuple[int]) -> None:
+        """Updates eligibilities, then the value function."""
 
-        # Used for visualization
-        # self.loss_history.append(history.history["loss"][0])  # type: ignore
-        self.epsilon_history.append(self.epsilon)
+        with tf.GradientTape(persistent=True) as tape:
+            target = reward + self._discount_factor * self.Q(tf.convert_to_tensor([successor_state]))  # type: ignore
+            prediction = self.Q(tf.convert_to_tensor([current_state]))
+            loss = self.Q.compiled_loss(target, prediction)
+            td_error = target - prediction
 
-        self.epsilon *= self.epsilon_decay_rate  # decay epsilon
+        gradients = tape.gradient(loss, self.Q.trainable_weights)
+        gradients = self.__modify_gradients(gradients, td_error)
+        self.Q.optimizer.apply_gradients(zip(gradients, self.Q.trainable_weights))  # type: ignore
+
+    def __modify_gradients(self, gradients, td_error):
+        for gradient, eligibility in zip(gradients, self.eligibilities):
+            gradient *= 1 / (2 * td_error)
+            eligibility = self.discount_factor * self.trace_decay * eligibility + gradient
+            gradient = td_error * eligibility
+        return gradients
+
+    def reset_eligibilities(self) -> None:
+        """Sets all eligibilities to 0.0"""
+        self.eligibilities = []
+        for weights in self.Q.trainable_weights:
+            self.eligibilities.append(tf.zeros(weights.shape))
 
     def __str__(self) -> str:
         return self.name
